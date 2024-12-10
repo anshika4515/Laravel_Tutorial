38fd7fdf16f2a64f6e81e3f4dd7c9fb670b831f5
anshika4515 <anshikaverma4515@gmail.com>
2024-12-10 10:18:33 +0530

    10 Dec | on blade file

--- laravel-tut/resources/views/welcome.blade.php
+++ laravel-tut/resources/views/welcome.blade.php
@@ -3,3 +3,25 @@
 <a href="/about">About Page</a>
 <!-- to use named routes -->
 <a href="{{route('mypost')}}">Post Page</a> 
+
+{{--some blade templates --}}
+{{5+2}}
+
+{!!"<h1>Hello</h1>"!!}
+
+
+@php
+ $users = ["abc" , "def" ,"ghi"]
+@endphp
+
+@foreach ( $users as $user )
+   <li>{{$loop->count}} - {{$user}} </li>
+@endforeach
+
+@foreach ( $users as $user )
+   @if($loop->first)
+      <li style="color:red">{{$user}}</li>
+   @else
+     <li>{{$user}}</li>
+     @endif
+@endforeach
