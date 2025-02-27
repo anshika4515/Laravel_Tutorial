e80f481e74e9d90cdde155bda97497eee46a27bb
anshika4515 <anshikaverma4515@gmail.com>
2025-02-27 17:03:42 +0530

    27 Feb | learn abt local and global scopes

--- model-event/app/Http/Controllers/UserController.php
+++ model-event/app/Http/Controllers/UserController.php
@@ -7,12 +7,23 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    // LOCAL SCOPE
+    // public function index(){
+    //     $user = User::with('post')
+    //            ->active()  //scope used here  //local scope
+    //            ->get();
+    //     return $user;
+    // }
+    
+    //GLOBAL SCOPE  -> no need to declare bcz as event it is using booted func generate automticlly 
     public function index(){
         $user = User::with('post')->get();
         return $user;
     }
     
-   
+
+    //we are using events here
     public function delete(){
         $user = User::find(4)->delete();
     }
--- model-event/routes/web.php
+++ model-event/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users',UserController::class);
+Route::resource('posts',PostController::class);
